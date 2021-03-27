b2c19e52d2e10e2b8e4be8d83c7f8f473f5fa87a
yasithWimukthi <it19966922@my.sliit.lk>
2021-03-27 12:30:45 +0530

    Implement create account screen

--- app/src/main/java/com/ninjacode98/journal/LoginActivity.java
+++ app/src/main/java/com/ninjacode98/journal/LoginActivity.java
@@ -2,13 +2,30 @@ package com.ninjacode98.journal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginButton;
+    Button createAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.email_sign_in_button);
+        createAccountButton = findViewById(R.id.create_account_button);
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,CreateAccountActivity.class));
+            }
+        });
     }
 }
