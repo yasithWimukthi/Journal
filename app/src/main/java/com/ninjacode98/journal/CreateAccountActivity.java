6cb5501147b51bdbe51ee6a0d38b95fd16a3ceca
yasithWimukthi <it19966922@my.sliit.lk>
2021-03-28 18:18:05 +0530

    setup create account with firebase

--- app/src/main/java/com/ninjacode98/journal/CreateAccountActivity.java
+++ app/src/main/java/com/ninjacode98/journal/CreateAccountActivity.java
@@ -3,12 +3,47 @@ package com.ninjacode98.journal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText usernameEditText;
+    private ProgressBar progressBar;
+    private Button createAccountButton;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    // FIRESTORE CONNECTION
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        createAccountButton = findViewById(R.id.create_account_button);
+        emailEditText = findViewById(R.id.email_account);
+        passwordEditText = findViewById(R.id.password_account);
+        usernameEditText = findViewById(R.id.username);
+        progressBar = findViewById(R.id.create_account_progress);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
     }
 }
--- app/src/main/java/com/ninjacode98/journal/LoginActivity.java
+++ app/src/main/java/com/ninjacode98/journal/LoginActivity.java
@@ -7,10 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
 
-    Button loginButton;
-    Button createAccountButton;
+    private Button loginButton;
+    private Button createAccountButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
