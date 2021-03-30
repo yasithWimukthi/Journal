b9ca145119db427d1f491af8518b4b45e481fd5b
yasithWimukthi <it19966922@my.sliit.lk>
2021-03-30 20:10:41 +0530

    Edit journal post class

--- app/src/main/java/com/ninjacode98/journal/PostJournalActivity.java
+++ app/src/main/java/com/ninjacode98/journal/PostJournalActivity.java
@@ -1,8 +1,11 @@
 package com.ninjacode98.journal;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,12 +24,14 @@ import util.JournalApi;
 
 public class PostJournalActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int GALLERY_CODE = 1;
     private Button saveButton;
     private ProgressBar progressBar;
     private ImageView addPhotoButton;
     private EditText titleEditText;
     private EditText thoughtsEditText;
     private TextView currentUserTextView;
+    private ImageView imageView;
 
     private String currentUsername;
     private String currentUserId;
@@ -40,6 +45,7 @@ public class PostJournalActivity extends AppCompatActivity implements View.OnCli
     private CollectionReference collectionReference = db.collection("Journal");
     private StorageReference storageReference;
 
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +64,7 @@ public class PostJournalActivity extends AppCompatActivity implements View.OnCli
         thoughtsEditText = findViewById(R.id.post_description_et);
         currentUserTextView = findViewById(R.id.post_username_textview);
         saveButton = findViewById(R.id.post_save_journal_button);
-        addPhotoButton = findViewById(R.id.postCameraButton);
+        imageView = findViewById(R.id.imageView);
 
         if(JournalApi.getInstance() != null){
             currentUsername = JournalApi.getInstance().getUsername();
@@ -91,11 +97,35 @@ public class PostJournalActivity extends AppCompatActivity implements View.OnCli
             case R.id.post_save_journal_button:
                 break;
             case R.id.postCameraButton:
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent,GALLERY_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_CODE && resultCode == RESULT_OK){
+            if(data != null){
+                imageUri = data.getData();
+                imageView.setImageURI(imageUri);
+            }
         }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        user = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(firebaseAuth != null){
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
