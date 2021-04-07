5b630b1b26e13741760708247bb187d5325285e0
yasithWimukthi <it19966922@my.sliit.lk>
2021-04-07 10:09:21 +0530

    create menu

--- app/src/main/java/com/ninjacode98/journal/JournalListActivity.java
+++ app/src/main/java/com/ninjacode98/journal/JournalListActivity.java
@@ -1,8 +1,11 @@
 package com.ninjacode98.journal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class JournalListActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class JournalListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                break;
+            case R.id.action_sign_out:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
