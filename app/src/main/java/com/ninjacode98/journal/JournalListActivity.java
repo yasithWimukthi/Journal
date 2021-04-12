128f5b0b486e72f192257ce2c4396906f5a486ff
yasithWimukthi <it19966922@my.sliit.lk>
2021-04-12 12:13:07 +0530

    modify recycle view

--- app/src/main/java/com/ninjacode98/journal/JournalListActivity.java
+++ app/src/main/java/com/ninjacode98/journal/JournalListActivity.java
@@ -2,17 +2,33 @@ package com.ninjacode98.journal;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Journal;
+import ui.JournalRecyclerAdapter;
+import util.JournalApi;
 
 public class JournalListActivity extends AppCompatActivity {
 
@@ -21,6 +37,12 @@ public class JournalListActivity extends AppCompatActivity {
     private FirebaseUser user;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private StorageReference storageReference;
+    private CollectionReference journalCollection = db.collection("Journal");
+
+    private List<Journal> journalList;
+    private RecyclerView recyclerview;
+    private JournalRecyclerAdapter journalRecyclerAdapter;
+    private TextView emptyList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +51,13 @@ public class JournalListActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
+
+        journalList = new ArrayList<>();
+
+        emptyList = findViewById(R.id.list_empty);
+        recyclerview = findViewById(R.id.recycler_view);
+        recyclerview.setHasFixedSize(true);
+        recyclerview.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
@@ -60,5 +89,31 @@ public class JournalListActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
+        journalCollection.whereEqualTo("userID", JournalApi.getInstance().getUserId())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if(!queryDocumentSnapshots.isEmpty()){
+
+                            for(QueryDocumentSnapshot journals : queryDocumentSnapshots){
+                                Journal journal = journals.toObject(Journal.class);
+                                journalList.add(journal);
+                            }
+
+                            journalRecyclerAdapter = new JournalRecyclerAdapter(JournalListActivity.this,journalList);
+                            recyclerview.setAdapter(journalRecyclerAdapter);
+                            journalRecyclerAdapter.notifyDataSetChanged();
+                        }else{
+                            emptyList.setVisibility(View.VISIBLE);
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
     }
 }
