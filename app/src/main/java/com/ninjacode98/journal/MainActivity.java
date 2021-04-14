9a2be24d8bf66b801aa05dae487ad7b210da94ae
yasithWimukthi <it19966922@my.sliit.lk>
2021-04-14 12:54:37 +0530

    final commit

--- app/src/main/java/com/ninjacode98/journal/MainActivity.java
+++ app/src/main/java/com/ninjacode98/journal/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ninjacode98.journal;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,9 +9,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import util.JournalApi;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button getStartedButton;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference usersCollection = db.collection("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +36,69 @@ public class MainActivity extends AppCompatActivity {
 
         getStartedButton = findViewById(R.id.startButton);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+
+                if(currentUser != null) {
+                    String userId = currentUser.getUid();
+
+                    usersCollection
+                            .whereEqualTo("userId",userId)
+                            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                @Override
+                                public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+
+                                    if (error != null){
+                                        return;
+                                    }
+
+                                    String name;
+
+                                    if(!value.isEmpty()){
+                                        for(QueryDocumentSnapshot snapshot : value){
+                                            JournalApi journalApi = JournalApi.getInstance();
+                                            journalApi.setUserId(snapshot.getString("userId"));
+                                            journalApi.setUsername(snapshot.getString("username"));
+
+                                            startActivity(new Intent(MainActivity.this,JournalListActivity.class));
+                                            finish();
+                                        }
+                                    }
+
+                                }
+                            });
+
+                }else{
+
+                }
+            }
+        };
+
         getStartedButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                finish();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (firebaseAuth != null){
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
